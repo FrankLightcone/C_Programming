0705dcea3b3bec22f9f42c14d09423fc08131064
FrankLightcone <threebody.beihai@qq.com>
2023-10-04 21:32:17 +0800

    2023-10-4

---
+++ C-009 Switch选择语句练习/C-009 Switch选择语句练习/C-009 Switch选择语句练习.cpp
@@ -0,0 +1,45 @@
+﻿// C-009 Switch选择语句练习.cpp 
+//
+
+#include <stdio.h>
+int a = 0;
+
+int main()
+{
+    printf("请选择《三体》中你最喜欢的角色（请输入序号）：\n1、章北海\n2、罗辑\n3、\
+维德\n4、大史\n5、云天明\n6、程心\n7、艾AA\n8、叶文洁\n9、汪淼\n");
+    scanf_s("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("没事的，都一样 成为军人的那一刻起，我就准备好了去往任何地方\n");
+        break;
+    case 2:
+        printf("人类不感谢罗辑，但我，面壁者罗辑，现在对三体世界说话\n");
+        break;
+    case 3:
+        printf("前进！不择手段的前进！\n");
+        break;
+    case 4:
+        printf("邪乎到家必有鬼\n");
+        break;
+    case 5:
+        printf("来了，爱了，送了她一颗星星，走了\n");
+        break;
+    case 6:
+        printf("我在攀爬责任的阶梯\n");
+        break;
+    case 7:
+        printf("我们度过了幸福的一生\n");
+        break;
+    case 8:
+        printf("不管怎样，我都尽了责任\n");
+        break;
+    case 9:
+        printf("虫子从来没有被真正战胜过！\n");
+        break;
+    }
+    if (a != 0) {
+        main();
+    }
+}
---
+++ C-011 暴力计算毕达哥拉斯三元组/C-011 暴力计算毕达哥拉斯三元组/C-011 暴力计算毕达哥拉斯三元组.cpp
@@ -0,0 +1,41 @@
+﻿// C-011 暴力计算毕达哥拉斯三元组.cpp 
+// 2023-10-4
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	// 方法一、暴力枚举法
+	// 原理：直接暴力计算所有组合
+	int a, b, c; a = b = c = 1;
+	int size = 10;
+	printf("请输入想要计算的范围：\n");
+	scanf_s("%d", &size);
+	printf("a\tb\tc\n");
+	//for (int i = 1; i <= size; i++) {
+	//	for (int o = i; o <= size; o++) { // 避免重复计算
+	//		for (int n = o; n <= size; n++) { // 避免掉不可能的c
+	//			if (i * i + o * o == n * n) {
+	//				if (n - o == 1 || o - i == 1) {
+	//					printf("%d\t%d\t%d\t%d\n", i, o, n, n * n);
+	//				}
+	//				else {
+	//					// printf("%d\t%d\t%d\t%d\n", i, o, n, n * n);
+	//				}
+	//			}
+	//		}
+	//	}
+	//}
+	// 方法二、求值判断法 实测快了许多！！！时间复杂度的原因，但是越到后面也会越慢
+	double v = 1;
+	for (int i = 1; i <= size; i++) {
+		for (int o = i; o <= size; o++) { // 避免重复计算
+			v = pow(pow(i, 2) + pow(o, 2), 0.5);
+			if (v - (int)v == 0) {
+				printf("%d\t%d\t%d\n", i, o, (int)v);
+			}
+		}
+	}
+
+}
